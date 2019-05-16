35d41ad6ea0ff28684c51579a6dae05aa3ccd6b8
xrc <1136375786@qq.com>
2019-05-16 19:22:32 +0800

    打印流

--- src/main/java/com/xu/day22__/Demo6_PrintStream.java
+++ src/main/java/com/xu/day22__/Demo6_PrintStream.java
@@ -2,30 +2,26 @@ package com.xu.day22__;
 
 import org.junit.Test;
 
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.PrintStream;
-import java.io.PrintWriter;
+import java.io.*;
 
 /**
  * 打印流
  * @see PrintStream 即System.out
- * @see PrintWriter
+ * @see PrintWriter#PrintWriter(Writer)  将文件字符写到控制台
  */
 public class Demo6_PrintStream {
-
     @Test
-    public void test(){
+    public void test() {
         PrintStream ps = System.out;
         ps.println(97);
 
         ps.write(97);
         ps.close();
-
     }
 
-    @Test public void test2() throws IOException {
-        PrintWriter pw = new PrintWriter(new FileWriter("fil"), true);
+    @Test
+    public void test2() throws IOException {
+        PrintWriter pw = new PrintWriter(new FileWriter("file.properties"), true);
         pw.println(100);
         pw.write(92);
         pw.close();
