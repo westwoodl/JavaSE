bc26f449994795599443d9c3ef1fd1e03865b061
xrc <1136375786@qq.com>
2019-05-16 17:41:04 +0800

    Scanner的基本原理

---
+++ src/main/java/com/xu/day22__/Demo7a_SystemInBufferedReader.java
@@ -0,0 +1,23 @@
+package com.xu.day22__;
+
+import org.junit.Test;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * System.in是一个字节流，使用转换流InputStreamReader转换为字符流
+ * 深入理解Scanner，Scanner更加强大
+ * @see InputStreamReader
+ */
+public class Demo7a_SystemInBufferedReader {
+
+    public static void main(String[] args) throws IOException {
+        try(
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            for (;;)
+                System.out.println(br.readLine());
+        }
+    }
+}
