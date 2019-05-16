86c08e5ef51fc2ad5a70ad207359a0b624977edf
xrc <1136375786@qq.com>
2019-05-16 16:59:01 +0800

    标准的输入输出流

--- src/main/java/com/xu/day22__/Demo7_SystemIO.java
+++ src/main/java/com/xu/day22__/Demo7_SystemIO.java
@@ -6,15 +6,15 @@ import java.io.IOException;
 import java.io.InputStream;
 
 /**
+ * 标准输入输出流
  * 不用close，没有和硬盘上的文件产生关系（IO）
  * @see System#in 单例模式
  * @see System#out PrintStream
  */
 public class Demo7_SystemIO {
-
-
     /**
      * Scanner 就是个包装类
+     *
      * @param args
      * @throws IOException
      */
@@ -22,10 +22,9 @@ public class Demo7_SystemIO {
         InputStream is = System.in;
 
         byte[] bytes = new byte[32];
-        for(;;) {
+        for (; ; ) {
             int x = is.read(bytes);
             System.out.println(new String(bytes, 0, x));
         }
     }
-
 }
