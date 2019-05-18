184f3420e9e4085fd3e9490e4569a7f506c1e369
xrc <1136375786@qq.com>
2019-05-18 11:43:47 +0800

    线程安全

--- src/main/java/com/xu/day24__Thread/Demo5_Threadsafety.java
+++ src/main/java/com/xu/day24__Thread/Demo5_Threadsafety.java
@@ -1,7 +1,15 @@
 package com.xu.day24__Thread;
 
+import java.util.Collection;
+
 /**
  * 多线程对同一数据进行操作
+ *
+ *     以往线程安全的类：
+ * @see java.util.Vector
+ * @see StringBuffer
+ * 将集合类变为线程安全的
+ * @see java.util.Collections#synchronizedCollection(Collection)
  */
 public class Demo5_Threadsafety {
     public static void main(String[] args) {
