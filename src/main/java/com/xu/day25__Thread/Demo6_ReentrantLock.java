04ce6978f254ec51638a9b728db81b48427cbf09
xrc <1136375786@qq.com>
2019-05-19 10:57:42 +0800

    重入锁 jdk1.5

--- src/main/java/com/xu/day25__Thread/Demo6_ReentrantLock.java
+++ src/main/java/com/xu/day25__Thread/Demo6_ReentrantLock.java
@@ -5,6 +5,7 @@ import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * 重入锁 jdk5新特性，一知半解...
+ * @see java.util.concurrent.CountDownLatch
  * @see ReentrantLock
  */
 public class Demo6_ReentrantLock {
@@ -23,49 +24,49 @@ public class Demo6_ReentrantLock {
         System.out.println(c2);
         System.out.println(c3);
         r.lock();
-            if (x != 1) {
-                c1.await();
-            }
-            System.out.print("1h");
-            System.out.print("H");
-            System.out.println("哈");
-            x = 2;
-            c2.signal();
+        if (x != 1) {
+            c1.await();
+        }
+        System.out.print("1h");
+        System.out.print("H");
+        System.out.println("哈");
+        x = 2;
+        c2.signal();
         r.unlock();
     }
 
     static void print2() throws InterruptedException {
         r.lock();
-            if (x != 2) {
-                c2.await();
-            }
-            System.out.print("2x");
-            System.out.print("X");
-            System.out.println("下");
-            x = 3;
-            c3.signal();
+        if (x != 2) {
+            c2.await();
+        }
+        System.out.print("2x");
+        System.out.print("X");
+        System.out.println("下");
+        x = 3;
+        c3.signal();
         r.unlock();
     }
 
     static void print3() throws InterruptedException {
         r.lock();
-            if (x != 3) {
-                c3.await();
-            }
-            System.out.print("3y");
-            System.out.print("Y");
-            System.out.println("有");
-            x = 1;
-            c1.signal();
+        if (x != 3) {
+            c3.await();
+        }
+        System.out.print("3y");
+        System.out.print("Y");
+        System.out.println("有");
+        x = 1;
+        c1.signal();
         r.unlock();
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
-                for (;;) {
+                for (; ; ) {
                     try {
                         print1();
                     } catch (InterruptedException e) {
@@ -74,10 +75,10 @@ public class Demo6_ReentrantLock {
                 }
             }
         }.start();
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
-                for (;;) {
+                for (; ; ) {
                     try {
                         print2();
                     } catch (InterruptedException e) {
@@ -86,10 +87,10 @@ public class Demo6_ReentrantLock {
                 }
             }
         }.start();
-        new Thread(){
+        new Thread() {
             @Override
             public void run() {
-                for (;;) {
+                for (; ; ) {
                     try {
                         print3();
                     } catch (InterruptedException e) {
