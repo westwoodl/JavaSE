ae2173e159794f77d7a2a0a822cc3cf652af0c65
xrc <1136375786@qq.com>
2019-05-21 17:19:05 +0800

    java7新特性

---
+++ src/main/java/com/xu/day27__JAVA/Demo4_jdk7.java
@@ -0,0 +1,23 @@
+package com.xu.day27__JAVA;
+
+import org.junit.Test;
+
+/**
+ * jdk7 新特性
+ */
+public class Demo4_jdk7 {
+    @Test
+    public void test(){
+
+        //1.写二进制
+        System.out.println(0b1101);
+        //2.写下划线
+        System.out.println(10_101_010_000L);
+        //3.switch可以用字符串
+        //4.菱形泛型
+        //5.多个异常合并
+        //6.自动关流
+
+
+    }
+}
