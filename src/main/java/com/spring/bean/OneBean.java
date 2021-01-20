2ef33e24dc1bb07c74c50d4bed701ba9b1b429b1
RongChao Xu <1136375786@qq.com>
2021-01-20 14:53:29 +0800

    Aop中Aspect的order：优先级越高越外层执行，越低越内层执行

---
+++ src/main/java/com/spring/bean/OneBean.java
@@ -0,0 +1,15 @@
+package com.spring.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author xu rongchao
+ * @date 2020-12-22 14:40
+ */
+@Component
+public class OneBean {
+    public OneBean() {
+        System.out.println("OneBean contract");
+    }
+
+}
